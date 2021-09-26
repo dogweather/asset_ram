79323fa810a17e89405d859801ed6f47e4af4b9c
Robb Shecter <robb@public.law>
2021-09-25 23:42:36 -0600

    specs pass

--- spec/asset_ram_spec.rb
+++ spec/asset_ram_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe AssetRam do
   it "has a version number" do
     expect(AssetRam::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
